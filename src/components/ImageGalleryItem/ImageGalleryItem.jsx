e18207f48f461dbfc898f8b044753b4315af57ef
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-09-27 23:25:30 +0200

    d

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.css';
 
 const ImageGalleryItem = ({ src, alt, onClick }) => (
@@ -6,5 +7,11 @@ const ImageGalleryItem = ({ src, alt, onClick }) => (
     <img src={src} alt={alt} />
   </li>
 );
+
+ImageGalleryItem.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default ImageGalleryItem;
